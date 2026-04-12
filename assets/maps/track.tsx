10fee111a1a07b3c96d7bc201ed05a7f69d92476
Matthieu Gagné <mathdaman@gmail.com>
2026-04-12 00:24:19 -0400

    feat: add tile type custom properties to track.tsx

--- assets/maps/track.tsx
+++ assets/maps/track.tsx
@@ -1,6 +1,49 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0"
-         name="track" tilewidth="8" tileheight="8"
-         spacing="0" margin="0" tilecount="9" columns="9">
+<tileset version="1.10" name="track" tilewidth="8" tileheight="8" tilecount="9" columns="9">
  <image source="tileset.png" width="72" height="8"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="TILE_WALL"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" value="TILE_ROAD"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="type" value="TILE_ROAD"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="type" value="TILE_SAND"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="type" value="TILE_OIL"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="type" value="TILE_BOOST"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="type" value="TILE_FINISH"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="type" value="TILE_ROAD"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="type" value="TILE_ROAD"/>
+  </properties>
+ </tile>
 </tileset>
